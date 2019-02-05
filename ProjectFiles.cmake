5444b8be4c294c5d45f06f336509329d78e9d508
Harikrishnan <harikrishnan.prabakaran@zohocorp.com>
2019-02-05 14:50:54 +0530

    Add files

---
+++ ProjectFiles.cmake
@@ -0,0 +1,15 @@
+# Copyright (c) <year> <author> (<email>) Distributed under the MIT License. See accompanying file
+# LICENSE.md or copy at http://opensource.org/licenses/MIT
+
+# Set project source files.
+set(SRC
+    "${SRC_PATH}/ThreadLocal.cpp"
+    "${SRC_PATH}/TraceLog.cpp"
+    "${SRC_PATH}/ParkingLot.cpp")
+
+# Set project benchmark files. set(BENCHMARK_SRC "${SRC_PATH}/benchmark.cpp")
+
+# Set project test source files.
+set(TEST_SRC
+    "${TEST_SRC_PATH}/testBase.cpp"
+    "${TEST_SRC_PATH}/testParkingLot.cpp")
---
+++ test/testParkingLot.cpp
@@ -0,0 +1,54 @@
+#include "ParkingLot.h"
+
+#include <iostream>
+#include <thread>
+
+#include "catch.hpp"
+
+enum ProdConsFlag
+{
+	PRODUCED,
+	CONSUMED
+};
+
+template <typename Data>
+using ParkingLot    = parking_lot::ParkingLot<Data>;
+using UnparkControl = parking_lot::UnparkControl;
+
+TEST_CASE("ParkingLot", "Futex - 2 Thread")
+{
+	ParkingLot<std::nullptr_t> parkinglot;
+	std::atomic<ProdConsFlag> flag = PRODUCED;
+
+	std::atomic_bool quit = false;
+	auto t                = std::thread{ [&flag, &quit, &parkinglot] {
+		while (!quit)
+		{
+			parkinglot.park(&flag,
+			                nullptr,
+			                [&quit, &flag]() { return !quit && flag == CONSUMED; },
+			                []() {});
+
+			if (!quit)
+				REQUIRE(flag == PRODUCED);
+
+			flag.store(CONSUMED);
+			parkinglot.unpark(&flag, [](auto) { return UnparkControl::RemoveBreak; });
+		}
+	} };
+
+	for (int i = 0; i < 100000; i++)
+	{
+		parkinglot.park(&flag, nullptr, [&flag]() { return flag == PRODUCED; }, []() {});
+
+		REQUIRE(flag == CONSUMED);
+
+		flag.store(PRODUCED);
+		parkinglot.unpark(&flag, [](auto) { return UnparkControl::RemoveBreak; });
+	}
+
+	quit = true;
+	parkinglot.unpark(&flag, [](auto) { return UnparkControl::RemoveBreak; });
+
+	t.join();
+}
