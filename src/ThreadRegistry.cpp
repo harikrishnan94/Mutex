ca323834ac2b467556d0e427b9c62e51c6f73c23
Harikrishnan Prabhakharan <harikrishnan.prabakaran@gmail.com>
2019-07-28 11:20:45 +0530

    Add ccache

--- src/ThreadRegistry.cpp
+++ src/ThreadRegistry.cpp
@@ -7,10 +7,10 @@
 
 namespace sync_prim {
 // Max used tid
-static std::atomic_int max_used_tid = -1;
+static std::atomic<int> max_used_tid = -1;
 
 // # Registered threads at any moment
-static std::atomic_int num_registerd_threads = 0;
+static std::atomic<int> num_registerd_threads = 0;
 
 // ID of calling thread,
 static thread_local int tid = -1;
